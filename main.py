dc65f8edbd81ca9b3de79b45ce52a61c94fc2b50
thomasbakker1977 <113938575+thomasbakker1977@users.noreply.github.com>
2023-01-13 21:00:28 +0100

    my first print example

---
+++ main.py
@@ -0,0 +1,8 @@
+# Do not modify these lines
+__winc_id__ = '05b076ade4d747878fa925e16e07a2c4'
+__human_name__ = 'print'
+
+# Add your code after this line
+print ('Learning to code is hard, but it is a lot of fun\n')
+print ('When print special characters like $ or % I think I have to use double quotes\n')
+print ('But no that\'s not how it works\n')
